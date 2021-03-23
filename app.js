c402b58d0e533069b99df92ba27e82c5369cce3c
bandilemashile <35614625+bandilemashile@users.noreply.github.com>
2021-03-23 16:32:13 +0200

    commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const app =  express();
+
+const fs = require("fs")
+const path = require("path")
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+const getAllFiles = function(dirPath, arrayOfFiles) {
+  files = fs.readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(function(file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+    }
+  })
+
+  app.get('/', (req,res) => {
+    res.send(arrayOfFiles)
+
+});
+  return arrayOfFiles
+}
+
+
+const result = getAllFiles("./tests")
+// [ "FILE_PATH", "FILE_PATH", "FILE_PATH" ]
+
+
+
+
+app.listen(3000);
